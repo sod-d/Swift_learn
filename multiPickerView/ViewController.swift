b08f7691721b8692cf2e75283a0ef53e42fcc6f2
sod-d <soduduracle@gmail.com>
2023-11-05 14:33:44 +0900

    Initial Commit

---
+++ multiPickerView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multiPickerView
+//
+//  Created by soyoung kim on 2023/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
